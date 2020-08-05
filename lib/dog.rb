0e6cb80c4f0a26361fea4e772370fad678fec8dd
wlcreate <waverley.nyc@gmail.com>
2020-08-05 17:14:46 -0400

    second commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class Dog
-  # code goes here
+
+  attr_accessor :owner, :mood, :name
+  attr_reader 
+
+  def initialize(name_par, mood_par, owner_par)
+    @name = name_par
+    @mood = mood_par
+    @owner = owner_par
+    #binding.pry
+  end
 end
+
+#binding.pry
+0
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,19 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor 
+
+  def initialize(name_par, species_par = "human")
+    @name = name_par
+    @species = species_par
+  end
+
+  def say_species
+    @species
+  end
+
+  def buy_cat(name)
+  end
+
+  def buy_dog
+  end
 end
